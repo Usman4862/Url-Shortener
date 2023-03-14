0365f52b4524e26830ea7feca9c4a3c9cc07dd1f
Usman4862 <um4862@gmail.com>
2023-03-14 14:06:12 +0500

    changes

--- router/views.py
+++ router/views.py
@@ -7,19 +7,17 @@ from django.views.generic import ListView
 
 # Create your views here.
 def home(request):
-    BASE_URL = request.get_raw_uri()
-    # return HttpResponse('Site is working')
+    BASE_URL = request.get_full_path()
     form = RouterForm(request.POST or None)
     if form.is_valid():
         form.save()
         key = form.cleaned_data.get('key')
         messages.success(request, f"URL has been successfully shortened to {BASE_URL}{key}")
         return redirect('home')
-    return render(request, 'router/home.html', {"form": form})
+    return render(request, 'router/homepage.html', {"form": form})
 
 def how_to(request):
-    # return HttpResponse("<h1>About Page</h1>")
-    return render(request, 'router/how_to_use.html')
+    return render(request, 'router/guide.html')
 
 class URLListView(ListView):
     model = Route
